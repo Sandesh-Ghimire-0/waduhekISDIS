524834398e2c60f773584bd45c42d51a8a40f79c
Sandesh <ghimiresandesh019@gmail.com>
2025-09-19 10:33:53 +0545

    port fixed

--- flask_app.py
+++ flask_app.py
@@ -36,4 +36,4 @@ def comment():
 if (__name__=='__main__'):
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=6969)
+    app.run(host="0.0.0.0", port=5000, debug=False)
